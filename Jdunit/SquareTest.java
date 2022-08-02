6f525b22a4233a98d4bab5cf373219b95913512b
Arshiya2698 <107995279+Arshiya2698@users.noreply.github.com>
2022-08-03 01:34:52 +0530

    first commit

---
+++ Jdunit/SquareTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SquareTest {
+
+	@Test
+	void test() {
+Testing s=new Testing();
+int output=s.square(2);
+assertEquals(4,output);
+
+	}
+
+}
